60b063565e64c6501646540ccf9eda25f94d9d31
Darel M <cls18drl@gmail.com>
2021-10-10 12:53:38 +0100

    s

---
+++ src/main/java/dad/maven/gson/gsonsample/Main.java
@@ -0,0 +1,38 @@
+package dad.maven.gson.gsonsample;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nombre,apellido;
+        int edad=-1;
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.println("Introduzca su nombre: ");
+            nombre = in.nextLine();
+        } while (nombre.isEmpty());
+        do {
+            System.out.println("Introduzca su apellido: ");
+            apellido = in.nextLine();
+        } while (apellido.isEmpty());
+        do {
+            try {
+                System.out.println("Introduzca su edad: ");
+                String aux = in.nextLine();
+                edad = Integer.parseInt(aux);
+            } catch (Exception e) {
+                System.out.println("No es un numero válido");
+            }
+        } while (edad < 0);
+        in.close();
+
+        Persona persona = new Persona(nombre, apellido, edad);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(persona);
+        System.out.println(json);
+    }
+}
---
+++ src/main/java/dad/maven/gson/gsonsample/Persona.java
@@ -0,0 +1,13 @@
+package dad.maven.gson.gsonsample;
+
+public class Persona {
+    String nombre;
+    String apellidos;
+    int edad;
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellido;
+        this.edad = edad;
+    }
+}
